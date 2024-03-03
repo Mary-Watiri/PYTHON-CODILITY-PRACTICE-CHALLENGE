48bd53073ba9c29de5a71f5bec6bba6b7e54db93
Mary Watiri <watirimary30@gmail.com>
2024-03-03 14:55:47 +0300

    Secon trial

--- sum.py
+++ sum.py
@@ -2,28 +2,22 @@
 #Define a function that has an array of values
 #Returns 
 
+def digit_sum(num):
+    return sum(int(digit) for digit in str(num))
 
-def solution(N):
-   if not (1 <= N <= 200000):
-       return "Invalid input"
-   alphabet = "abcdefghijklmnopqrstuvwxyz"
+def solution(A):
+    max_sum = -1
+    num_dict = {}
+    for num in A:
+        digits = [int(d) for d in str(num)]  # Split the number into its digits
+        sum_digits = sum(digits)  # Calculate the sum of digits
+        if sum_digits in num_dict:
+            max_sum = max(max_sum, num + num_dict[sum_digits])
+        num_dict[sum_digits] = max(num, num_dict.get(sum_digits, -1))
+    return max_sum
 
-   unique_letters = (N + 1) // 2  # Adding 1 ensures an equal number of occurrences
+# Test cases
+print(solution([51, 71, 17, 42]))  # Output: 93
+print(solution([42, 33, 60]))      # Output: 102
+print(solution([51, 32, 43]))
 
-   # Create the string with equal occurrences of unique letters
-
-   result = ""
-
-   for i in range(unique_letters):
-
-    #    result += alphabet[i] * 2  # Each letter occurs twice
-
-   # If N is odd, add one more letter to make occurrences equal
-
-   if N % 2 == 1:
-
-       result += alphabet[unique_letters]
-
-   return result
-
-print(solution(5))
